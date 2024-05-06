add softmax and fix loss

develop_resnet_FCN.py
@@ -8,6 +8,7 @@ import numpy as np
 from tqdm import tqdm
 from utils.data_utils import *
 from utils.model_utils import *
+from sklearn.metrics import jaccard_score
 
 
 def create_argparser() -> argparse.Namespace:
@@ -15,18 +16,18 @@ def create_argparser() -> argparse.Namespace:
     defines the command line argument parser
     '''
     parser = argparse.ArgumentParser()
-    parser.add_argument("-pretrained", action="store_true") 
-    parser.add_argument("-num_classes", type=int)
-    parser.add_argument("-batch_size")
-    parser.add_argument("-img_size", default=(224, 224))
-    parser.add_argument("-patience")
-    parser.add_argument("-result_dir", type=Path)
-    parser.add_argument("-train_result_filename", type=str)
-    parser.add_argument("-test_result_filename", type=str)
-    parser.add_argument("-lr", type=float)
-    parser.add_argument("-model_save_name")
-    parser.add_argument("-num_epochs")
-    parser.add_argument("-data_root")
+    parser.add_argument("-pretrained", action="store_true", default=True) 
+    parser.add_argument("-num_classes", type=int, default=37)
+    parser.add_argument("-batch_size", default=10)
+    parser.add_argument("-img_size", default=(128, 128))
+    parser.add_argument("-patience", default=5)
+    parser.add_argument("-result_dir", type=Path, default=Path("C:\\personal_ML\\basic-semantic-segmentation\\results\\"))
+    parser.add_argument("-train_result_filename", type=str, default="model_1_train_results.json")
+    parser.add_argument("-test_result_filename", type=str, default="model_1_test_results.json")
+    parser.add_argument("-lr", type=float, default=1e-3)
+    parser.add_argument("-model_save_name", default="model_1.pth.tar")
+    parser.add_argument("-num_epochs", default=256)
+    parser.add_argument("-data_root", default=Path("C:\\personal_ML\\Oxford_PyTorch\\"))
     parser.add_argument("-continue_bool", action="store_true", default=False)
     parser.add_argument("-start_epoch", type=int, default=0)
     parser.add_argument("-weight_path")
@@ -46,16 +47,36 @@ def validate(
     with torch.no_grad():
         model.eval() 
         val_loss = 0
-        for batch_image, batch_mask in tqdm(val_loader, desc="Validating"):
+        for batch_image, batch_mask, batch_class in tqdm(val_loader, desc="Validating"):
             batch_pred = model(batch_image.to(device))["out"]
-            batch_onehot_segmask  = one_hot_mask(mask=batch_mask[0], num_classes=num_classes).unsqueeze(dim=0)
-            for seg_mask in batch_mask[1:]:
-                one_hot_seg = one_hot_mask(mask=seg_mask, num_classes=num_classes).unsqueeze(dim=0)
-                batch_onehot_segmask = np.concatenate([batch_onehot_segmask, one_hot_seg], axis=0)
-            batch_onehot_segmask = torch.tensor(batch_onehot_segmask).to(device).to(torch.float64)
+            batch_onehot_segmask = batch_onehot(batch_mask=batch_mask, batch_class=batch_class, num_classes=num_classes).to(device)
             val_loss += criterion(batch_pred, batch_onehot_segmask).cpu().detach().numpy()
     val_loss /= len(val_loader)
     return val_loss
+
+
+def onehot_tensor(tensor, num_classes, class_idx):
+    new_ten = torch.empty(size=[num_classes, tensor.shape[0], tensor.shape[1]])
+    for x_pos in range(tensor.shape[0]):
+        for y_pos in range(tensor.shape[1]):
+            new_ten[:, x_pos, y_pos] = torch.nn.functional.one_hot(input=class_idx, num_classes=num_classes)
+    return new_ten
+
+
+def onehot_multi(tensor, num_classes):
+    for x_pos in range(tensor.shape[1]):
+        for y_pos in range(tensor.shape[2]):
+            oh_chan = tensor[:, x_pos, y_pos]
+            oh_chan = torch.nn.functional.one_hot(input=torch.argmax(oh_chan), num_classes=num_classes)
+            tensor[:, x_pos, y_pos] = oh_chan
+    return tensor
+
+
+def batch_onehot(batch_mask, batch_class, num_classes):
+    new_batch = torch.empty(size=(batch_mask.shape[0], num_classes, batch_mask.shape[1], batch_mask.shape[2]))
+    for batch_idx in range(batch_mask.shape[0]):
+        new_batch[batch_idx, :, :, :] = onehot_tensor(batch_mask[batch_idx], num_classes, batch_class[batch_idx])    
+    return new_batch
 
 
 def train(
@@ -90,23 +111,18 @@ def train(
                 break
             else:
                 epoch_loss = 0
-                for batch_image, batch_seg_mask in tqdm(train_loader, desc="Training"):
-                    model.train()
-                    batch_pred = model(batch_image.to(device))["out"]
-                    batch_onehot_segmask  = one_hot_mask(
-                        mask=batch_seg_mask[0], 
-                        num_classes=num_classes
-                        ).unsqueeze(dim=0)
-                    for seg_mask in batch_seg_mask[1:]:
-                        one_hot_seg = one_hot_mask(mask=seg_mask, num_classes=num_classes).unsqueeze(dim=0)
-                        batch_onehot_segmask = np.concatenate([batch_onehot_segmask, one_hot_seg], axis=0)
-                    batch_onehot_segmask = torch.tensor(batch_onehot_segmask).to(device).to(torch.float32)
+                model.train()
+                for batch_image, batch_seg_mask, batch_class in tqdm(train_loader, desc="Training"):
+                    batch_pred = model(batch_image.to(device))["out"].softmax(dim=1)
+
+                    batch_onehot_segmask = batch_onehot(batch_mask=batch_seg_mask, batch_class=batch_class, num_classes=num_classes).to(device)
 
                     loss = criterion(batch_pred, batch_onehot_segmask)
-                    epoch_loss += loss.cpu().detach().numpy()
                     optimizer.zero_grad()
                     loss.backward()
                     optimizer.step()
+                    epoch_loss += loss.cpu().detach().numpy()
+
 
                 train_loss_list.append((epoch_idx+1, epoch_loss/len(train_loader)))
                 val_loss = validate(val_loader, model, criterion=criterion, device=device, num_classes=num_classes)
@@ -133,39 +149,44 @@ def test_model(
     measures model performance on the test dataset
     '''
     with torch.no_grad():
-        model = load_model(weight_path=result_dir.joinpath(model_save_name), model=model)
+        # model = load_model(weight_path=result_dir.joinpath(model_save_name), model=model)
         model.eval()
         test_dict = {
             "IoU": 0,
-            "Dice": 0
         }
-        for batch_image, batch_seg_mask in tqdm(test_loader, desc="Testing"):
+        for batch_image, batch_seg_mask, batch_class in tqdm(test_loader, desc="Testing"):
             batch_iou = 0
-            batch_dice = 0
             batch_pred = model(batch_image.to(device))["out"]
-            batch_onehot_segmask  = one_hot_mask(
-                mask=batch_seg_mask[0], 
-                num_classes=num_classes
-                ).unsqueeze(dim=0)
-            for seg_mask in batch_seg_mask[1:]:
-                one_hot_seg = one_hot_mask(mask=seg_mask, num_classes=num_classes).unsqueeze(dim=0)
-                batch_onehot_segmask = np.concatenate([batch_onehot_segmask, one_hot_seg], axis=0)
-            batch_onehot_segmask = torch.tensor(batch_onehot_segmask).to(device).to(torch.float64)
-            for pred_idx in range(len(batch_pred)):
-                pred = torch.nn.Softmax(dim=1)(batch_pred[pred_idx])
-                mask = batch_onehot_segmask[pred_idx]
-                pred = onehot_3d_array(tensor=pred, num_classes=num_classes)
 
-                mean_dice, mean_iou = measure_dice_and_iou(
-                    prediction_mask=pred, 
-                    ground_truth_mask=mask
-                    )
-                batch_iou += mean_iou
-                batch_dice += mean_dice
+            batch_onehot_segmask = batch_onehot(
+                batch_mask=batch_seg_mask, batch_class=batch_class, num_classes=num_classes
+            )
+           
+            for pred_idx in range(len(batch_pred)):
+                if num_classes == 1:
+                    pred = torch.nn.Sigmoid()(batch_pred[pred_idx]).cpu().numpy()
+                    pred = np.round(pred)
+                else:
+                    pred = torch.nn.Softmax(dim=0)(batch_pred[pred_idx])
+                    pred = onehot_multi(tensor=pred, num_classes=num_classes).int().cpu().numpy()
+
+                mask = batch_onehot_segmask[pred_idx, :, :, :].int().cpu().numpy()
+
+                avg_iou = 0
+                for class_idx in range(num_classes):
+                    avg_iou += jaccard_score(y_true=mask[class_idx].flatten(), y_pred=pred[class_idx].flatten(), average="binary")
+                avg_iou /= num_classes
+                batch_iou += avg_iou
+
+            batch_iou /= len(batch_pred)
+            print("batch IoU", batch_iou)
             test_dict["IoU"] += batch_iou
-            test_dict["Dice"] += batch_dice
         test_dict["IoU"] /= len(test_loader)
-        test_dict["Dice"] /= len(test_loader)
+        print("test results", test_dict)
+
+
+        test_dict["IoU"] += batch_iou
+        test_dict["IoU"] /= len(test_loader)
         print("test results", test_dict)
         return test_dict 
     